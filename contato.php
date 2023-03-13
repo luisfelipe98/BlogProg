b291a9ed83c3d1da3fec21634358a369bc5d52b6
luisfelipe98 <luisfelipemm98@gmail.com>
2023-03-13 17:35:51 -0300

    Adicionando os templates de footer e header

--- contato.php
+++ contato.php
@@ -1,7 +1,7 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Olá Pessoal!</h1>
+    <h1>Página de Contato</h1>
 <?php
     include_once("templates/footer.php");
 ?>
---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ESTILOS DO PROJETO -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+    <title>Blog Codar</title>
+</head>
+<body>
+    <header>
+        Template cabeçalho
+    </header>
+</body>
+</html>
