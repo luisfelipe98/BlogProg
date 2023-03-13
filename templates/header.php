245d4513473345a48ba3d4d68ee2cd7a3b49e60f
luisfelipe98 <luisfelipemm98@gmail.com>
2023-03-13 17:46:32 -0300

    Adicionando o navbar no header.php

--- templates/header.php
+++ templates/header.php
@@ -17,7 +17,17 @@
 </head>
 <body>
     <header>
-        Template cabeçalho
+        <a href="<?= $BASE_URL ?>" id="logo">
+            <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Logo do site"> 
+        </a>
     </header>
+    <nav>
+        <ul id="navbar">
+            <li><a href="<?= $BASE_URL ?>">Home</a></li>
+            <li><a href="#">Categorias</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="<?= $BASE_URL ?>contato.php">Contato</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
