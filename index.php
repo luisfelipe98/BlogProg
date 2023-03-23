04c96b92fbae71635ebe2b8629cfd6ac46b71da0
luisfelipe98 <luisfelipemm98@gmail.com>
2023-03-23 16:55:56 -0300

    Finalizando o CSS da página index.php

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
     include_once("templates/header.php");
 ?>
     <main>
-        <div id="title-container">
+        <div class="title-container">
             <h1>Blog Codar</h1>
             <p>O seu blog de programação</p>
         </div>
-        <div id="posts-container">
+        <div class="posts-container">
             <?php foreach ($posts as $post) : ?>
-                <div id="post-box">
+                <div class="post-box">
                     <img src="<?= $BASE_URL ?>/img/<?= $post["img"] ?>" alt="<?= $post["title"] ?>">
-                    <h2 id="post-title">
+                    <h2 class="post-title">
                         <a href="<?= $BASE_URL ?>post.php?id=<?= $post["id"] ?>"><?= $post["title"] ?></a>
                     </h2>
                     <p class="post-description"><?= $post["description"] ?></p>
-                </div>
-                <div class="tags-container">
-                    <?php foreach ($post["tags"] as $tag) : ?>
-                        <a href="#"><?= $tag ?></a>    
-                    <?php endforeach; ?>    
+                    <div class="tags-container">
+                        <?php foreach ($post["tags"] as $tag) : ?>
+                            <a href="#"><?= $tag ?></a>    
+                        <?php endforeach; ?>    
+                    </div>
                 </div>        
             <?php endforeach; ?>    
         </div>
